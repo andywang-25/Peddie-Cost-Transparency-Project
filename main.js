bf0df0f92fe186ef5cf6f5473f8b3e4e76969069
Tiger Li <116858086+tigr-10@users.noreply.github.com>
2023-11-09 09:16:00 -0500

    Add files via upload
    Somewhat working: classes + cost show up when clicked and disappear when clicked again. 
    
    Not: cumulative, bugs with recognizing two teachers from one class as the same

--- main.js
+++ main.js
@@ -1,98 +1,233 @@
 
-function filterClasses() {
-    const input = document.querySelector("input[type='text']");
-    const filter = input.value.toLowerCase();
-    const ul = document.getElementById("classesList");
-    const li = ul.getElementsByTagName("li");
 
-    for (let i = 0; i < li.length; i++) {
-        let label = li[i].getElementsByTagName("label")[0];
-        let txtValue = label.textContent || label.innerText;
-        if (txtValue.toLowerCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+const textbookData = []; // Create an array to store book information
+const selectedCourses = new Set(); // Create a Set to store selected course names
+
+fetch("./data/textbooks.csv")
+    .then((response) => response.text())
+    .then((data) => {
+        Papa.parse(data, {
+            delimiter: ",",
+            header: true,
+            dynamicTyping: true,
+            skipEmptyLines: true,
+            ltrim: true,
+            complete: function (results) {
+                const textbookData1 = results.data;
+                // Loop through each row of data and extract the desired information for each book
+                textbookData1.forEach((textbook) => {
+                    const bookInfo = {
+                        department: textbook['Department'],
+                        courseName: textbook['Course Name'],
+                        title: textbook['Title'],
+                        buyLink: textbook['buy link'],
+                        price: textbook['book price'],
+                        teachers: textbook['Teacher'],
+                    };
+                    textbookData.push(bookInfo);
+                });
+                // Now, you have an array containing book information for each book
+                console.log(textbookData);
+                alert("Book information is available. Check the console for details.");
+                // Call the function to generate the checklist after the data is loaded
+                generateChecklist();
+            },
+        });
+    });
+
+// Function to generate the checklist items
+function generateChecklist() {
+    console.log("generating checklist")
+    const classesList = document.getElementById('classesList');
+    const clearRectangle = document.querySelector("#column_selected_Classes .clear_rectangle");
+    const costsContainer = document.querySelector("#column_Costs .clear_rectangle");
+
+    // Create a dictionary to store checklist items by course and teacher
+    const checklistItems = {};
+
+    // Loop through the textbookData array and group items by course and teacher
+    textbookData.forEach((textbook) => {
+        const key = `${textbook.courseName}_${textbook.teachers}`;
+
+        if (!checklistItems[key]) {
+            checklistItems[key] = {
+                courseName: textbook.courseName,
+                teachers: textbook.teachers,
+                textbooks: [], // Create an array to store associated textbooks
+            };
+        }
+        checklistItems[key].textbooks.push(textbook); // Store associated textbooks
+    });
+
+    // Generate checklist items from the grouped data
+    for (const key in checklistItems) {
+        if (checklistItems.hasOwnProperty(key)) {
+            const item = checklistItems[key];
+            const listItem = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = item.courseName;
+            const label = document.createElement('label');
+            label.htmlFor = item.courseName;
+            label.textContent = `${item.courseName} (Teacher: ${item.teachers})`;
+
+            listItem.appendChild(checkbox);
+            listItem.appendChild(label);
+
+            classesList.appendChild(listItem);
+
+
+            // Add a click event to show associated textbooks and costs when the label is clicked
+            label.addEventListener('click', () => {
+              if (selectedCourses.has(item.courseName)) {
+
+                checkbox.checked = true; // Uncheck the checkbox
+
+                item.textbooks.forEach((textbook) => {
+                    const textbookElement = document.createElement("div");
+                    textbookElement.textContent = `Textbook: ${textbook.title} (Cost: $${textbook.price})`;
+                    handleCourseSelection(item.courseName, textbook.price, false); // Manually call unselect handler\
+                }); 
+
+            
+              } else {
+
+
+                  clearRectangle.textContent = ''; // Clear the previous selection
+                  costsContainer.textContent = ''; // Clear the previous costs
+          
+                  // Display the selected course in the "Selected Courses" column
+                  const selectedCourseItem = document.createElement("div");
+                  selectedCourseItem.textContent = item.courseName;
+                  selectedCourseItem.style.fontSize = "22px"; // Set the font size
+                  clearRectangle.appendChild(selectedCourseItem);
+          
+                  // Display the associated textbooks and their costs
+                  item.textbooks.forEach((textbook) => {
+                      const textbookElement = document.createElement("div");
+                      textbookElement.textContent = `Textbook: ${textbook.title} (Cost: $${textbook.price})`;
+                      textbookElement.style.fontSize = "18px"; // Set the font size
+                      costsContainer.appendChild(textbookElement);
+                  });
+          
+                  // Add the selected course to the Set
+                  selectedCourses.add(item.courseName);
+              }
+          });
+          
         }
     }
+    console.log(checlistItems)
+
+
+    // function populateOptions(){
+    //     var select = document.getElementById("classesList");
+    //     select.innerHTML='';
+    //     for(var i=0; i<checklistItems.length;i++){
+    //         var option = document.createElement("class");
+    //         option.value=checklistItems[i];
+    //         option.text=checklistItems[i];
+    //         select.appendChild(option);
+    //     }
+    // }
+    
+    // populateOptions();
+
+    // function searchOptions(){
+    //     var input,filter, select, classes, clase, i;
+
+    //     input=document.getElementById("searchBar");
+    //     filter=input.value.toUpperCase();
+    //     select=document.getElementById("classesList");
+    //     classes=select.classes;
+
+    //     for(i=0;i<classes.length;i++){
+    //         clase=classes[i];
+    //         if(clase.text.toUpperCase().indexOf(filter) > -1){
+    //             clase.style.display="";
+    //         }
+    //         else{
+    //             clase.style.display="none";
+             
+    //         }
+    //     }
+    // }
+
+  function clearRectangleContains(text) {
+    let children = clearRectangle.children;
+    for (let i = 0; i < children.length; i++) {
+      if (children[i].textContent === text) {
+        return true;
+      }
+     }
+     return false;
+   }
+   
+
+  function removeSelectedClass(courseName) {
+    let children = clearRectangle.children;
+    for (let i = 0; i < children.length; i++) {
+      if (extractCourseName(children[i].textContent) === courseName) {
+        clearRectangle.removeChild(children[i]);
+        return;
+      }
+    }
+  }
+
+
+  function removeCost(price) {
+
+    console.log("removing the price right now, the price is: " + price)
+    const costElements = costsContainer.querySelectorAll("div");
+    costElements.forEach((element) => {
+      if (element.textContent.includes(price)) {
+        costsContainer.removeChild(element);
+        return;
+      }
+    });
+    // Ensure the costsContainer is not empty after removal
+    if (!costsContainer.textContent.trim()) {
+      costsContainer.textContent = ''; // Clear any remaining text
+    }
+  }
+
+  function extractCourseName(text) {
+        // Extract the course name from the text
+        const match = text.match(/^(.*?) \(Teacher: .*\)$/);
+        return match ? match[1] : text;
+      }
+
+  function handleCourseSelection(courseName, price, isChecked) {
+    if (isChecked) {
+      if (!selectedCourses.has(courseName)) {
+        const selectedCourseItem = document.createElement("div");
+        selectedCourseItem.textContent = courseName;
+        selectedCourseItem.style.fontSize = "22px"; // Set the font size
+        clearRectangle.appendChild(selectedCourseItem);
+
+       // Find the matching textbooks for the selected class
+       const selectedTextbooks = textbookData.filter((textbook) => textbook.courseName === courseName);
+       const selectedTextbooks2 = textbookData.filter((textbook) => textbook.price === price);
+
+       // Add the selected course to the Set
+       selectedCourses.add(courseName);
+     }
+   } else { //else, if the user unclicks the selected course, we want to remove it from the selected courses and costs column
+    console.log("we are now beginning to move the class")
+        removeSelectedClass(courseName);
+        removeCost(price);
+
+        // Remove the selected course from the Set
+        selectedCourses.delete(courseName);
+
+   }
+ }
 }
 
-/*
-
-document.addEventListener("DOMContentLoaded", function () {
-    const ul = document.getElementById("classesList");
-    const li = ul.getElementsByTagName("li");
-    const clearRectangle = document.querySelector("#column_selected_Classes .clear_rectangle");
-
-    for (let i = 0; i < li.length; i++) {
-        const checkbox = li[i].querySelector("input[type='checkbox']");
-
-        checkbox.addEventListener("change", function () {
-            if (checkbox.checked) {
-                let label = li[i].getElementsByTagName("label")[0];
-                let txtValue = label.textContent || label.innerText;
-
-                //alert("Selected class: " + txtValue);
-
-                // Create a new div for the selected class
-                const selectedCourseItem = document.createElement("div");
-                selectedCourseItem.textContent = txtValue;
-
-                // Append the selected class to the "clear_rectangle" div
-                clearRectangle.appendChild(selectedCourseItem);
-            }
-        });
-    }
-});
-*/
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const ul = document.getElementById("classesList");
-    const li = ul.getElementsByTagName("li");
-    const clearRectangle = document.querySelector("#column_selected_Classes .clear_rectangle");
 
-    for (let i = 0; i < li.length; i++) {
-        const checkbox = li[i].querySelector("input[type='checkbox']");
-        let label = li[i].getElementsByTagName("label")[0];
-        let txtValue = label.textContent || label.innerText;
-
-        checkbox.addEventListener("change", function () {
-            if (checkbox.checked) {
-                // Check if a duplicate is not already added
-                if (!clearRectangleContains(txtValue)) {
-                    // Create a new div for the selected class
-                    const selectedCourseItem = document.createElement("div");
-                    selectedCourseItem.textContent = txtValue;
-                    selectedCourseItem.style.fontSize = "30px"; // Set the font size
+  
 
 
-                    // Append the selected class to the "clear_rectangle" div
-                    clearRectangle.appendChild(selectedCourseItem);
-                }
-            } else {
-                // Remove the text when the checkbox is unchecked
-                removeSelectedClass(txtValue);
-            }
-        });
-    }
 
-    function clearRectangleContains(text) {
-        let children = clearRectangle.children;
-        for (let i = 0; i < children.length; i++) {
-            if (children[i].textContent === text) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function removeSelectedClass(text) {
-        let children = clearRectangle.children;
-        for (let i = 0; i < children.length; i++) {
-            if (children[i].textContent === text) {
-                clearRectangle.removeChild(children[i]);
-                return;
-            }
-        }
-    }
-});
